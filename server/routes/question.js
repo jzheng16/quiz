06e1bd1c166ad58827ed023850d10a91732f7db7
Joey Zheng <jxzheng16@gmail.com>
2018-09-08 07:27:39 -0400

    Update question.js
    refactor using await/async

--- server/routes/question.js
+++ server/routes/question.js
@@ -4,22 +4,24 @@ const Question = require('../db/models/question');
 router.get('/', async (req, res) => {
   try {
     const questions = await Question.find({});
-    res.send(questions);
+    res.json(questions);
   } catch (err) {
-    throw err;
+    res.status(404).send(err);
   }
 });
 
 
-router.post('/', (req, res) => {
-  Question.create({
-    title: 'Question Numba 1',
-    answer: '42',
-    body: 'What is the meaning of life?'
-  }, (err, newQuestion) => {
-    if (err) console.log('err');
+router.post('/', async (req, res) => {
+  try {
+    let newQuestion = await Question.create({
+      title: 'Question Numba 1',
+      answer: '42',
+      body: 'What is the meaning of life?'
+    });
     res.json(newQuestion);
-  });
+  } catch (err) {
+    res.status(404).send(err);
+  }
 });
 
 module.exports = router;
